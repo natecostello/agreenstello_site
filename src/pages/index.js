1e0bae9eab08ec0f756460b2b925c61ff8e4f669
Nate Costello <natecostello@gmail.com>
2021-06-02 21:21:27 +0000

    landing page based on mdx file

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,25 +5,39 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 
-export default IndexPage
+export default function IndexPage({ data }) {
+  const { mdx } = data
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <MDXRenderer>{mdx.body}</MDXRenderer>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    mdx(frontmatter: { level: { eq: 0 } }) {
+      id
+      body
+      frontmatter {
+        title
+      }
+    }
+  }
+`
