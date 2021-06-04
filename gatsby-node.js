5000a7df3fac455b47aa655083c41664a209e917
Nate Costello <natecostello@gmail.com>
2021-06-04 02:02:10 +0000

    added page generation for categories

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,47 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  //   if (node.internal.type === "Mdx" && node.frontmatter.level === 1) {
+  if (node.internal.type === "Mdx") {
+    const slug = createFilePath({ node, getNode, basePath: `content` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+// create category pages
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMdx(filter: { frontmatter: { level: { eq: 1 } } }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMdx.edges.forEach(({ node }) => {
+      createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/category.js`),
+          context: {
+              // data that the template needs
+              slug: node.fields.slug,
+          },
+      })
+  });
+}
---
+++ src/templates/category.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { graphql } from "gatsby"
+
+export default function CategoryPageTemplate({ data }) {
+  const { mdx } = data
+  return (
+    <div>
+      <h1>{mdx.frontmatter.title}</h1>
+      <MDXRenderer>{mdx.body}</MDXRenderer>
+    </div>
+  )
+}
+
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      body
+      frontmatter {
+          title
+      }
+    }
+  }
+`
