57ad46074e634a662286825bf221b96ad6a9cce9
Nate Costello <natecostello@gmail.com>
2021-06-06 21:21:14 +0000

    reformated to function from arrow

--- src/components/header.js
+++ src/components/header.js
@@ -3,35 +3,39 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import Navbar from "./Navbar"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+export default function Header({ siteTitle }) {
+  return (
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
+        // position: `sticky`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-        <Navbar />
-      </h1>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+          // position: `sticky`,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+          <Navbar />
+        </h1>
+      </div>
     </div>
-  </header>
-)
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -41,4 +45,4 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+//export default Header
