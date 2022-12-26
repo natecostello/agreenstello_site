7a087d68063c3781c1460c539dfb4095b0b58cce
Nate Costello <natecostello@gmail.com>
2022-12-26 02:06:29 +0000

    deleted comment functionality

--- src/components/PostMeta/index.js
+++ src/components/PostMeta/index.js
@@ -2,7 +2,6 @@ import { CommentCount } from 'disqus-react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Comments } from 'styled-icons/fa-solid'
 import { Timer } from 'styled-icons/material'
 import { Calendar } from 'styled-icons/octicons'
 import { disqusConfig } from 'templates/post'
@@ -19,13 +18,6 @@ const PostMeta = ({ title, slug, date, timeToRead, tags, inTitle = false }) => (
       <Timer size="1.2em" />
       &ensp;
       {timeToRead} min read
-    </span>
-    <span>
-      <Comments size="1.2em" />
-      &ensp;
-      <Link to={slug + `#disqus_thread`}>
-        <CommentCount {...disqusConfig({ slug, title })} />
-      </Link>
     </span>
     <TagList tags={tags} />
   </Meta>
