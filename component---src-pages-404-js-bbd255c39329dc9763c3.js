d1fa064c994a72cbb5e811fff31d137ba2413116
natecostello <natecostello@users.noreply.github.com>
2025-04-09 23:47:52 +0000

    deployed via Gatsby Publish Action 🎩 for 20d6920d4f072b85f1996f2bec6951d382a5fd37

---
+++ component---src-pages-404-js-bbd255c39329dc9763c3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(t,e,n){n.r(e);var a=n(7294),u=n(262);e.default=function(){return a.createElement(a.Fragment,null,a.createElement(u.Z,{title:"404: Not found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bbd255c39329dc9763c3.js.map
